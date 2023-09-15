a65d29d2df05474f989265da121e3cea4161744b
isel06002 <isel06002@E-PW0234HX>
2023-09-15 12:25:07 +0200

    Add project files.

---
+++ Uppgift 2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Uppgift2.__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter din favorit bok?..");
+            string bok =Console.ReadLine();
+            Console.WriteLine("Din favorit bok är "+bok);
+            Console.ReadKey();
+        }
+    }
+}
